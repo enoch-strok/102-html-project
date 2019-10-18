dc5087b3e85d7a634cdac06ce4815086b1a0968c
enoch-strok <enochstrok@outlook.com>
2019-10-18 09:25:50 -0700

    added javascript to site

---
+++ app.js
@@ -0,0 +1,54 @@
+
+// Name Function
+function askName() {
+    var username = prompt('What is your name?');
+  
+    return '<h3>' + username + ', ' + '</h3>';
+  }
+ 
+  
+
+// Greeting Function PART A
+  function greetingA() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+
+  
+    if (hourNow > 18) {
+      greeting = 'Good evening,';
+ 
+  
+    } else if (hourNow > 12) {
+      greeting = 'Good afternoon, ';
+  
+    } else if (hourNow > 0) {
+      greeting = 'Good morning, ';
+    } else {
+      greeting = 'Welcome, ';
+  
+    }
+    return '<h3>' + greeting + '</h3>';
+  }
+
+
+  // Greeting Function PART B
+  function greetingB() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greetingTwo;
+  
+    if (hourNow > 18) {
+      greetingTwo = 'Contact us tonight!';
+  
+    } else if (hourNow > 12) {
+      greetingTwo = 'Contact us this afternoon!';
+  
+    } else if (hourNow > 0) {
+      greetingTwo = 'Contact us this morning!';
+    } else {
+      greetingTwo = 'Just call us!';
+  
+    }
+    return '<h3>' + greetingTwo + '</h3>';
+  }
